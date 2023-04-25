24e22c44da671944ff88702e5da4752c9203c93f
Zakhar <99883153+imbator@users.noreply.github.com>
2023-04-26 00:44:29 +0300

    Automate updated
    In process (need some fixes)

--- DSP_Test4.py
+++ DSP_Test4.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy
 import matplotlib.pyplot as plt
+import time
 
 # Служебные переменные
 LAST_FIGURE_NUMBER = 0
@@ -25,18 +26,14 @@ class ShiftRegister:
     """ Описание класса сдвигового регистра."""
     def  __init__(self, size):
         self.size = size # Установка размера регистра
-        self.reg = np.zeros(size) # Хранилище регистра
+        self.reg = [0] * size # Хранилище регистра
         self.output = self.reg[-1] # На выход всегда поступает последний элемент
 
     def make_shift(self, add_value):
         """Имитация сдвига регистра, ex: 0 1 0 (clk)--> 0 0 1"""
+        # print(f"add value: {add_value}")
         self.reg[1:] = self.reg[0:len(self.reg) - 1] # Сдвиг
         self.reg[0] = add_value # Добавление в начало
-    def __str__(self):
-        return ".".join(self.reg)
-
-    # def __call__(self, *args, **kwargs):
-    #     return self.reg
 
 class Switch:
     """ Описание класса свича."""
@@ -49,6 +46,11 @@ class Switch:
 
     def set_input_values(self, value_1, value_2):
         """Установка входных значений и получение выходных"""
+        if self.update_counter == self.update_frequency:
+            self.inverted_output = not self.inverted_output
+            self.update_counter = 0
+
+
         self.input_1 = value_1
         self.input_2 = value_2
         if not self.inverted_output:
@@ -57,10 +59,8 @@ class Switch:
         else:
             self.output_1 = self.input_2
             self.output_2 = self.input_1
+
         self.update_counter += 1
-        if self.update_counter == self.update_frequency:
-            self.inverted_output = not self.inverted_output
-            self.update_counter = 0
 
 class BPFThreadAutomate:
     """ Описывает автомат, работаюший по поточной реализации."""
@@ -83,6 +83,10 @@ class BPFThreadAutomate:
         self.SW2 = Switch(1) # Второй свитч
         self.current_butterfly_multiplier_index = 0 # Множитель бабочки
         self.n = 0 # Регулятор ключа
+
+        # Интерфейс ввода-вывода
+        self.bpf_input_1 = 0
+        self.bpf_input_2 = 0
         self.bpf_output_1 = 0 # Первый выход из автомата
         self.bpf_output_2 = 0 # Второй выход из автомата
 
@@ -103,27 +107,37 @@ class BPFThreadAutomate:
             элементов автомата друг с другом."""
 
         # Множители для бабочки
-        current_butterfly_1_mul = self.BUTTERFLY_1_M_VALUES[self.current_butterfly_multiplier_index]
-        current_butterfly_2_mul = self.BUTTERFLY_1_M_VALUES[self.current_butterfly_multiplier_index]
-        current_butterfly_3_mul = self.BUTTERFLY_1_M_VALUES[self.current_butterfly_multiplier_index]
-        self.current_butterfly_multiplier_index += 1
         self.check_current_butterfly_index()
+        current_butterfly_1_mul = self.BUTTERFLY_1_M_VALUES[self.current_butterfly_multiplier_index]
+        current_butterfly_2_mul = self.BUTTERFLY_2_M_VALUES[self.current_butterfly_multiplier_index]
+        current_butterfly_3_mul = self.BUTTERFLY_3_M_VALUES[self.current_butterfly_multiplier_index]
 
-        # Логика того, что подается на вход автомата
-        if self.n <= 3:
-            # Ключ замкнут на регистр
-            self.reg_0.make_shift(self.signal_data[self.n])
-            self.n += 1
+        self.current_butterfly_multiplier_index += 1
+
+        # Ввод-вывод:
+        if 0 <= self.n <= 3:
+            self.bpf_input_1 = self.signal_data[self.n]
+            self.bpf_input_2 = 0
+        elif 3 < self.n <= 7:
+            self.bpf_input_1 = 0
+            self.bpf_input_1 = self.signal_data[self.n]
         else:
-            # Ключ замкнут на вторую линию
-            self.reg_0.make_shift(0)
-        self.check_n()
+            self.bpf_input_1 = 0
+            self.bpf_input_2 = 0
 
-        # Обновление состояния нулевого регистра
-        self.reg_1.make_shift((self.reg_0.reg[-1] - self.signal_data(self.n)) * current_butterfly_1_mul)
+        # Обновлeние состояния нулевого регистра:
+        self.reg_0.make_shift(self.bpf_input_1)
+
+        # Обновление состояния первого регистра
+        self.reg_1.make_shift((self.reg_0.reg[-1] - self.bpf_input_2) * current_butterfly_1_mul)
 
         # Обновление состояния первого свича:
-        self.SW1.set_input_values(self.reg_1.reg[-1], self.signal_data(self.n) - self.reg_0.reg[-1])
+        # TODO: переделать! (тут должна быть бабочка)
+        print(f"To set SW1 values: { self.reg_0.reg[-1]}")
+        self.SW1.set_input_values(self.bpf_input_2 + self.reg_0.reg[-1], self.reg_1.reg[-1])
+
+        #Увеличиваем счетчик
+        self.n += 1
 
         # Обновление состояния второго регистра:
         self.reg_2.make_shift(self.SW1.output_1)
@@ -132,13 +146,49 @@ class BPFThreadAutomate:
         self.reg_3.make_shift((self.reg_2.reg[-1] - self.SW1.output_2) * current_butterfly_2_mul)
 
         # Обновление состояния второго свича:
-        self.SW2.set_input_values(self.reg_3.reg[-1], self.SW1.output_2 + self.reg_2.reg[-1])
+        # TODO: переделать! (тут тоже должна быть бабочка lol)
+        self.SW2.set_input_values(self.SW1.output_2 + self.reg_2.reg[-1], self.reg_3.reg[-1])
 
         # Обновление пятого регистра:
-        self.reg_4.make_shift(self.SW2.output_2)
+        self.reg_4.make_shift(self.SW2.output_1)
 
-        # TODO: запустить поточную реализацию
+        # Значения на выходе автомата:
+        self.bpf_output_1 = self.reg_4.reg[-1] + self.SW2.output_2
+        self.bpf_output_2 = (self.reg_4.reg[-1] - self.SW2.output_2) * current_butterfly_3_mul
 
+def threads_bpf_realisation(signal_discretes) -> list:
+    """Поточный метод расчета БПФ."""
+    result = []
+    print(f"Входные данные поточной реализации: {signal_discretes}")
+    automate = BPFThreadAutomate(signal_discretes) # Создаем автомат для работы
+    clk_cycles = 0
+    while clk_cycles != 6:
+        automate.update()
+        print(f"Automate updated. Current cycle: {clk_cycles}")
+
+        print(f"input 1: {automate.bpf_input_1}")
+        print(f"input 2: {automate.bpf_input_2}")
+        print(f"Reg 0 value: {automate.reg_0.reg}")
+        print(f"Reg 1 value: {automate.reg_1.reg}")
+        print(f"SW 1: input 1: {automate.SW1.input_1}, input 2: {automate.SW1.input_2},"
+              f" output 1: {automate.SW1.output_1}, output 2: {automate.SW1.output_2}, "
+              f"is inverted: {automate.SW1.inverted_output}")
+        print(f"Reg 2 value: {automate.reg_2.reg}")
+        print(f"Reg 3 value: {automate.reg_3.reg}")
+        print(f"SW 2: input 1: {automate.SW2.input_1}, input 2: {automate.SW2.input_2},"
+              f" output 1: {automate.SW2.output_1}, output 2: {automate.SW2.output_2}, "
+              f"is inverted: {automate.SW2.inverted_output}")
+        print(f"Reg 4 value: {automate.reg_4.reg}")
+        print("|================DATA OUTPUT=================|")
+        print(f"Data getted from J: {automate.bpf_output_1}")
+        print(f"Data geteed from K: {automate.bpf_output_2}")
+        print(" ")
+
+        clk_cycles += 1
+        time.sleep(0.1)
+
+
+    return result
 
 
 # Построение фигуры
@@ -147,7 +197,6 @@ def make_figure() -> plt.figure:
     global LAST_FIGURE_NUMBER
     LAST_FIGURE_NUMBER += 1
     return plt.figure(LAST_FIGURE_NUMBER)
-
 # Расчет бабочки в номере 3
 def butterfly_right(seq):
     """ Расчет методом бабочки фурье при прореживании справа."""
@@ -235,14 +284,6 @@ def calculate_butterfly_section_left(base_column: list, new_column: list,
         new_column[i + size] = base_column[i] - base_column[i + size] *  e_values[e_counter]
         e_counter += 1
 
-def threads_bpf_realisation(signal_discretes):
-    """Поточный метод расчета БПФ."""
-    print(f"Входные данные поточной реализации: {signal_discretes}")
-    automate = BPFThreadAutomate() # Создаем автомат для работы
-    print(automate)
-
-
-
 if __name__ == '__main__':
     # Task 3
     n = sympy.symbols('n')
@@ -323,5 +364,4 @@ if __name__ == '__main__':
 
 
     # plt.show()
-
 
